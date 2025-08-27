a47f6b5b40c0af8579673fbf85c3222df9525c92
Jadestar01 <jadestar0904@gmail.com>
2025-08-27 23:09:01 +0900

    Remove Dummy

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -13,40 +13,9 @@ public class Test : MonoBehaviour
 
     private void Start()
     {
-        //button.onClick.AddListener(OnClick);
     }
 
     public void OnClick()
     {
-        if (tableCenter == null)
-        {
-            Debug.LogError("TableCenter가 할당되지 않았습니다!");
-            return;
-        }
-    
-        var iconTable = tableCenter.GetTable<IconAssetTableSO>();
-        if (iconTable == null)
-        {
-            Debug.LogError("IconAssetTableSO가 TableCenter에 등록되지 않았습니다!");
-            return;
-        }
-    
-        Debug.Log(iconTable.HasAsset("Coal"));
-    
-        var data = iconTable.GetAsset("Coal");
-        if (data == null)
-        {
-            Debug.LogError("'Coal' 아이콘을 찾을 수 없습니다!");
-            return;
-        }
-
-        if (image == null)
-        {
-            Debug.LogError("Image이 null임");
-            return;
-        }
-
-        image.sprite = data;
     }
-
 }
